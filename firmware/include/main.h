e2844c428633ecf9dfe0e2d8a67606e522247cb9
Mauro Brandão <mrbrjr@gmail.com>
2025-09-14 08:35:28 -0300

    Remove C++ defines from main.h

--- firmware/include/main.h
+++ firmware/include/main.h
@@ -1,17 +1,9 @@
 #ifndef __MAIN_H
 #define __MAIN_H
 
-#ifdef __cplusplus
-extern "C" {
-#endif
- 
 #include "stm32f4xx_hal.h"
   
 void Error_Handler(void);
- 
-#ifdef __cplusplus
-}
-#endif
-
+  
 /* __MAIN_H */
 #endif 
